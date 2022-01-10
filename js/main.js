d0688b598b3e1e01d6eb25fcaab84f0d1555c640
Asal <shamimasal8@gmail.com>
2022-01-10 20:08:16 +0100

    1

--- js/main.js
+++ js/main.js
@@ -21,3 +21,14 @@ const navMenu = document.getElementById('nav-menu'),
 
     }
 
+    /* Remove Menu Mobile */
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
